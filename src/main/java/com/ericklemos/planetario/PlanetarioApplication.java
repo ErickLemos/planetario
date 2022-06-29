9ad861c7a757b185d77dc8cfd766e29334b8785c
Erick B. Lemos <ericklemoscosta@gmail.com>
2022-06-28 22:52:24 -0300

    commit inicial

---
+++ src/main/java/com/ericklemos/planetario/PlanetarioApplication.java
@@ -0,0 +1,13 @@
+package com.ericklemos.planetario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanetarioApplication.class, args);
+    }
+
+}
